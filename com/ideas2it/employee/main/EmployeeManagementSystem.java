5b26f4a52e5afca011afb08e536d4f240bcb02b2
unknown <ananth.karnan@ideas2it.com>
2022-09-13 15:22:56 +0530

    'theory'

--- com/ideas2it/employee/main/EmployeeManagementSystem.java
+++ com/ideas2it/employee/main/EmployeeManagementSystem.java
@@ -8,7 +8,7 @@ import com.ideas2it.employee.view.EmployeeView;
  * This Application used to maintain the employee details.
  * Create, read, update and delete operations were done in this application.
  * @version 2.0 13-09-2022.
- * @author  Ananth K.
+ * @author  Ananth Karnan.
  */
 public class EmployeeManagementSystem {
 
